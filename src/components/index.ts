6a2cced5d9085500bf2bc579479c8356646fb957
rivaldo rizky <rvldrh>
2024-07-11 21:12:35 +0700

    rework 1 publish npm

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,3 +1,3 @@
 // components/index.ts
-export { default as Button1 } from './button/button';
-export { default as Input } from './input/input';
+export { default as Button1 } from './button/button.tsx';
+export { default as Input } from './input/input.tsx';
